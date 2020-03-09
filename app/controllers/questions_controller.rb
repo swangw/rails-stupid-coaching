667953bec5fdb7f4844c10843e4963845bd60165
Saken <swangwio@gmail.com>
2020-03-08 23:59:12 -0400

    challenge 2

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,13 @@
+class QuestionsController < ApplicationController
+  def ask; end
+
+  def answer
+    @question = params[:question]
+    @answer = "I don't care, get dressed and go to work!"
+    if @question == 'I am going to work'
+      @answer = 'Great!'
+    elsif @question[-1] == '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    end
+  end
+end
